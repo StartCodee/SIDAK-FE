38844798b40cfa993014db62fd6f2d10fbce1cdd
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-07-19 23:23:08 +0700

    feat(add new pasar kecamatan kabupaten and comodity)

---
+++ app/admin/commodity-management/page.tsx
@@ -0,0 +1,292 @@
+
+"use client";
+
+import * as React from "react";
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    SortingState,
+    VisibilityState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import DefaultLayout from "@/components/admin/Layouts/DefaultLayout";
+import Breadcrumb from "@/components/admin/Breadcrumbs/Breadcrumb";
+import { useState } from "react";
+
+
+import CheckboxFive from "@/components/admin/Checkboxes/CheckboxFive";
+import CheckboxFour from "@/components/admin/Checkboxes/CheckboxFour";
+import CheckboxOne from "@/components/admin/Checkboxes/CheckboxOne";
+import CheckboxThree from "@/components/admin/Checkboxes/CheckboxThree";
+import CheckboxTwo from "@/components/admin/Checkboxes/CheckboxTwo";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+
+export type Commodity = {
+    nama: string;
+};
+
+
+// Sample data
+const data: Commodity[] = [
+    
+    {
+        nama: "Beras Premium",
+
+    },
+    {
+        nama: "Daging Premium",
+
+    },
+
+    {
+        nama: "Beras Premium",
+
+    },
+    {
+        nama: "Daging Premium",
+
+    },
+    {
+        nama: "Beras Premium",
+
+    },
+    {
+        nama: "Daging Premium",
+
+    },
+    {
+        nama: "Beras Premium",
+
+    },
+    {
+        nama: "Daging Premium",
+
+    },
+
+    {
+        nama: "Beras Premium",
+
+    },
+    {
+        nama: "Daging Premium",
+
+    },{
+        nama: "Beras Premium",
+
+    },
+    {
+        nama: "Daging Premium",
+
+    },
+    // Add other commodities similarly...
+];
+
+
+
+export default function Home() {
+
+    // Define columns based on the data structure
+    const columns: ColumnDef<Commodity>[] = [
+        {
+            accessorKey: "commodity",
+            header: "No",
+        },
+        {
+            accessorKey: "nama",
+            header: "Nama Komoditas",
+        },
+
+    ];
+
+
+    const [activeTab, setActiveTab] = useState('profile');
+
+    const handleTabClick = (tab: string): void => {
+        setActiveTab(tab);
+    };
+
+    const [sorting, setSorting] = React.useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
+    const [rowSelection, setRowSelection] = React.useState({});
+
+    const table = useReactTable({
+        data,
+        columns,
+        onSortingChange: setSorting,
+        onColumnFiltersChange: setColumnFilters,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            columnFilters,
+            columnVisibility,
+            rowSelection,
+        },
+    });
+
+
+
+    return (
+        <>
+            <DefaultLayout>
+                <Breadcrumb pageName="Management Commodity" />
+
+                <div className="col-span-3 bg-white rounded-lg border border-stroke bg-white px-5  py-10 shadow-default">
+                    <div className={`w-full  ${activeTab === 'profile' ? '' : 'hidden'}`}>
+                        <h1 className="text-2xl font-bold">Management Commodity</h1>
+                        <div className="flex items-center py-4">
+                            <Input
+                                placeholder="Filter commodities..."
+                                value={(table.getColumn("commodity")?.getFilterValue() as string) ?? ""}
+                                onChange={(event) =>
+                                    table.getColumn("commodity")?.setFilterValue(event.target.value)
+                                }
+                                className="max-w-sm"
+                            />
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button variant="outline" className="ml-auto">
+                                        Columns <ChevronDown className="ml-2 h-4 w-4" />
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <div className="ms-2">
+                                <Dialog >
+                                    <DialogTrigger asChild>
+                                        <Button variant="outline">Add Commodity</Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="sm:max-w-[425px]">
+                                        <DialogHeader>
+                                            <DialogTitle>Add Commodity</DialogTitle>
+                                        </DialogHeader>
+                                        <div className="grid gap-4 py-4">
+                                            <div className="grid grid-cols-4 items-center gap-4">
+                                                <Label htmlFor="name" className="text-right">
+                                                    Name
+                                                </Label>
+                                                <Input
+                                                    id="name"
+                                                    defaultValue="Pedro Duarte"
+                                                    className="col-span-3"
+                                                />
+                                            </div>
+                                        </div>
+                                        <DialogFooter>
+                                            <Button type="submit">Save changes</Button>
+                                        </DialogFooter>
+                                    </DialogContent>
+                                </Dialog>
+
+                                </div>
+                            </DropdownMenu>
+                        </div>
+                        <div className="rounded-md border">
+                            <Table>
+                                <TableHeader>
+                                    {table.getHeaderGroups().map((headerGroup) => (
+                                        <TableRow key={headerGroup.id}>
+                                            {headerGroup.headers.map((header) => (
+                                                <TableHead key={header.id}>
+                                                    {header.isPlaceholder ? null : flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                                </TableHead>
+                                            ))}
+                                        </TableRow>
+                                    ))}
+                                </TableHeader>
+                                <TableBody>
+                                    {table.getRowModel().rows?.length ? (
+                                        table.getRowModel().rows.map((row,key) => (
+                                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+
+                                                    <TableCell>
+                                                        {key + 1}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {row.original.nama}
+                                                    </TableCell>
+
+                                            </TableRow>
+                                        ))
+                                    ) : (
+                                        <TableRow>
+                                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                                No results.
+                                            </TableCell>
+                                        </TableRow>)}
+                                </TableBody>
+
+                            </Table>
+                        </div>
+                        <div className="flex items-center justify-end space-x-2 py-4">
+                            <div className="flex-1 text-sm text-muted-foreground">
+                                {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                                {table.getFilteredRowModel().rows.length} row(s) selected.
+                            </div>
+                            <div className="space-x-2">
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => table.previousPage()}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    Previous
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => table.nextPage()}
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    Next
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </DefaultLayout>
+        </>
+    );
+}
