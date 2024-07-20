43fbebafdec3369a454457e6fcfbafab3fe6c62f
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-07-20 09:23:12 +0700

    add slicing berita

--- app/berita/[id]/page.tsx
+++ app/berita/[id]/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 import Navbar from '@/components/ui/navbar';
 import Image from 'next/image';
 import Background from '@/public/bgg.png';
@@ -20,42 +21,42 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import berita from '@/public/berita.png';
 import berita2 from '@/public/berita 2.png';
 import berita3 from '@/public/berita 3.png';
+import { useRouter } from "next/navigation";
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
-const BeritaPage: React.FC = () => {
-	const data = [
-		{
-			Image: berita,
-			id: 1,
-			title:
-				'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 1',
-			content:
-				'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-		{
-			image: berita2,
-			id: 2,
-			title:
-				'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 2',
-			content:
-				'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-		{
-			image: berita3,
-			id: 3,
-			title:
-				'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 3',
-			content:
-				'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-		{
-			image: berita,
-			id: 4,
-			title:
-				'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 4',
-			content:
-				'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-	];
+
+
+export default function Page({ params }: { params: { id: string } }) {
+	const router = useRouter();
+
+	const [berita, setBerita] = useState<any>({});
+
+
+	const getBerita = async () => {
+		try {
+		  const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/api/news/${params.id}`, {
+			headers: {
+			  'content-type': 'application/json',
+			  'Authorization': `Bearer ${localStorage.getItem('token')}`,
+			},
+		  });
+		  setBerita(response.data);
+		} catch (error) {
+		  Swal.fire({
+			icon: 'error',
+			title: 'Error terjadi',
+			text: 'Mohon coba lagi nanti.',
+			showConfirmButton: false,
+			timer: 1500,
+		  });
+		}
+	  };
+
+	  useEffect(() => {
+		getBerita();
+	}, []);
+
 	return (
 		<>
 			<Navbar />
@@ -104,52 +105,22 @@ const BeritaPage: React.FC = () => {
     <CardContent className="flex flex-col gap-4 px-4 sm:px-10 md:px-30 lg:px-50 pb-20">
         <div className="flex flex-col lg:flex-row mb-5 ">
             <Image
-                src={data[0].Image}
-                alt={data[0].title}
+                src={`${process.env.NEXT_PUBLIC_BACKEND_HOST}/api/image/${berita.image}`}
+                alt={berita.title}
                 width={1000}
                 height={300}
                 className="rounded-2xl mr-15"
             />
             <div>
                 <h1 className="text-lg sm:text-xl md:text-2xl mb-10 text-left font-bold">
-                    Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG
-                    Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga
-                    Pangan
+                    {berita.title}
                 </h1>
                 <div className="h-1 rounded-lg mt-10 bg-black z-0"></div>
             </div>
         </div>
         <div>
             <p className="text-justify text-sm md:text-base lg:text-lg leading-relaxed text-gray-700">
-                Pada Kamis, 2 Februari 2024, Kantor Perwakilan Bank Indonesia
-                Provinsi Sulawesi Tengah bersama BULOG Provinsi Sulawesi Tengah
-                melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan
-                ini adalah untuk memastikan ketersediaan beras mencukupi dan
-                mencegah kenaikan harga pangan, khususnya beras.
-                <br /> <br />
-                Tim dari Bank Indonesia dan BULOG memeriksa kondisi stok beras,
-                mengevaluasi jumlah persediaan, dan memastikan kualitas beras
-                yang disimpan sesuai standar. Kepala Kantor Perwakilan Bank
-                Indonesia Sulawesi Tengah menekankan pentingnya sinergi antara
-                berbagai pihak dalam menjaga stabilitas harga pangan. BULOG
-                terus berupaya menjaga pasokan beras dengan langkah strategis,
-                termasuk importasi beras jika diperlukan dan optimalisasi
-                produksi dalam negeri.
-                <br /> <br />
-                Kegiatan pengecekan ini mendapat sambutan positif dari pedagang
-                dan konsumen, yang merasa lebih tenang dengan jaminan
-                ketersediaan beras. Seorang pedagang di Pasar Induk Palu
-                menyatakan bahwa kepastian pasokan beras penting untuk menjaga
-                harga tetap stabil dan terjangkau bagi konsumen. "Jika stok
-                beras cukup, kami sebagai pedagang juga tidak khawatir dengan
-                fluktuasi harga yang bisa merugikan," katanya.
-                <br /> <br />
-                Diharapkan, dengan langkah preventif ini, harga beras di
-                Sulawesi Tengah tetap stabil, tidak membebani masyarakat, dan
-                menjadi contoh bagi daerah lain dalam menjaga ketersediaan dan
-                stabilitas harga pangan. Sinergi antara berbagai pihak
-                diharapkan dapat terus berlanjut demi kesejahteraan masyarakat
-                dan ketahanan pangan nasional.
+                {berita.content}
             </p>
         </div>
     </CardContent>
@@ -191,4 +162,3 @@ const BeritaPage: React.FC = () => {
 	);
 };
 
-export default BeritaPage;
--- app/berita/page.tsx
+++ app/berita/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import Navbar from '@/components/ui/navbar';
 import Image from 'next/image';
 import Background from '@/public/bgg.png';
@@ -11,46 +12,145 @@ import {
 	CardContent,
 	CardDescription,
 	CardFooter,
-
 } from '@/components/ui/card';
+import Swal from "sweetalert2";
+import axios from "axios";
 import { Button } from '@/components/ui/button';
 import bank from '@/public/bank.svg';
 import sulaw from '@/public/sulaw.svg';
 import {
-	MagnifyingGlassIcon,
-} from '@heroicons/react/24/outline';
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "@/components/ui/pagination"
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
-import berita from '@/public/berita.png';
+import Link from "next/link";
+
 import berita2 from '@/public/berita 2.png';
 import berita3 from '@/public/berita 3.png';
 
+interface News {
+	id: number;
+	title: string;
+	content: string;
+	author_id: number;
+	created_at: string;
+	updated_at: string;
+	deleted_at: string | null;
+	image: string | null;
+}
+
+interface PaginationInfo {
+	current_page: number;
+	from: number;
+	last_page: number;
+	first_page_url: string;
+	next_page_url: string | null;
+	prev_page_url: string | null;
+	last_page_url: string;
+	path: string;
+	per_page: number;
+	to: number;
+	total: number;
+	links: {
+		url: string | null;
+		label: string;
+		active: boolean;
+	}[];
+}
+
 const BeritaPage: React.FC = () => {
-	const data = [
-		{
-			Image: berita,
-			id: 1,
-			title: 'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 1',
-			content: 'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-		{
-			image : berita2,
-			id: 2,
-			title: 'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 2',
-			content: 'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-		{
-			image : berita3,
-			id: 3,
-			title: 'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 3',
-			content: 'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-		{
-			image : berita,
-			id: 4,
-			title: 'Kantor Perwakilan Bank Indonesia Sulawesi Tengah Bersama BULOG Lakukan Pengecekan Stok Beras untuk Cegah Kenaikan Harga Pangan 4',
-			content: 'Pada Kamis, 2 februari 2024 kantor perwakilan bank indonesia provinsi sulawesi tengah, melakukan pengecekan stok beras di gudang BULOG. Tujuan kegiatan ini adalah. ',
-		},
-	]
+	const [berita, setBerita] = useState<News[]>([]);
+	const [paginationInfo, setPaginationInfo] = useState<PaginationInfo | null>(null);
+
+	const getBerita = async (page: number = 1, limit: number = 2) => {
+		try {
+			const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/api/news?page=${page}&limit=${limit}`, {
+				headers: {
+					'content-type': 'application/json',
+					'Authorization': `Bearer ${localStorage.getItem('token')}`,
+				},
+			});
+			if (response.data.data) {
+				setBerita(response.data.data);
+				setPaginationInfo(response.data.paginationInfo);
+			}
+		} catch (error: any) {
+			if (error.response && error.response.status === 401) {
+				Swal.fire({
+					icon: 'error',
+					title: error.response.data.message,
+					showConfirmButton: false,
+					timer: 1500
+				});
+			} else {
+				Swal.fire({
+					icon: 'error',
+					title: 'error terjadi',
+					text: 'mohon coba lagi nanti.',
+					showConfirmButton: false,
+					timer: 1500
+				});
+			}
+		}
+	};
+
+	const searchBerita = async () => {
+		const search = document.getElementById('search') as HTMLInputElement;
+		if (search.value) {
+			try {
+				const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/api/news?search=${search.value}`, {
+					headers: {
+						'content-type': 'application/json',
+						'Authorization': `Bearer ${localStorage.getItem('token')}`,
+					},
+				});
+				if (response.data.data) {
+					setBerita(response.data.data);
+					setPaginationInfo(response.data.paginationInfo);
+				}
+			} catch (error: any) {
+				if (error.response && error.response.status === 401) {
+					Swal.fire({
+						icon: 'error',
+						title: error.response.data.message,
+						showConfirmButton: false,
+						timer: 1500
+					});
+				} else {
+					Swal.fire({
+						icon: 'error',
+						title: 'error terjadi',
+						text: 'mohon coba lagi nanti.',
+						showConfirmButton: false,
+						timer: 1500
+					});
+				}
+			}
+		} else {
+			getBerita();
+		}
+	};
+
+	useEffect(() => {
+		getBerita();
+	}, []);
+
+	const handlePageChange = (url: string | null) => {
+		if (url) {
+			const urlParams = new URLSearchParams(url.split('?')[1]);
+			const page = urlParams.get('page');
+			if (page) {
+				getBerita(Number(page));
+			}
+		}
+	};
+
 	return (
 		<>
 			<Navbar />
@@ -81,9 +181,10 @@ const BeritaPage: React.FC = () => {
 					<input
 						type="text"
 						placeholder="Cari Berita"
+						id='search'
 						className="w-full outline-none rounded-full"
 					/>
-					<Button className="ml-2 bg-blue-600">
+					<Button onClick={searchBerita} className="ml-2 bg-blue-600">
 						<MagnifyingGlassIcon className="h-6 w-6" />
 					</Button>
 				</div>
@@ -97,13 +198,13 @@ const BeritaPage: React.FC = () => {
 						</h1>
 					</CardHeader>
 					<CardContent className="flex flex-col gap-4">
-						{data.map((item) => (
+						{berita.map((item) => (
 							<Card
 								key={item.id}
 								className="w-full flex flex-col lg:flex-row items-center mb-4">
 								<CardHeader className="w-full lg:w-1/3">
 									<Image
-										src={item.image || berita}
+										src={`${process.env.NEXT_PUBLIC_BACKEND_HOST}/api/image/${item.image}`}
 										className="rounded-2xl w-full"
 										alt="berita"
 										width={350}
@@ -119,10 +220,53 @@ const BeritaPage: React.FC = () => {
 										<div className="my-4 border-b border-black w-full" />
 										<p className="text-lg lg:text-base ">{item.content}</p>
 									</div>
-									<Button className="mt-4">Baca Selengkapnya</Button>
+									<Link href={`/berita/${item.id}`}>
+										<Button className="mt-4">Baca Selengkapnya</Button>
+									</Link>
+
 								</CardContent>
 							</Card>
 						))}
+
+						<div className="flex justify-end">
+							<Pagination>
+								<PaginationContent>
+									{paginationInfo?.links.map((link, index) => {
+										if (link.label == 'Next &raquo;') {
+											return (
+												<PaginationItem key={index}>
+													<PaginationNext
+														href="#"
+														onClick={() => handlePageChange(paginationInfo?.next_page_url as string)}
+													/>
+												</PaginationItem>
+											);
+										} else if (link.label == '&laquo; Previous') {
+											return (
+												<PaginationItem key={index}>
+													<PaginationPrevious
+														href="#"
+														onClick={() => handlePageChange(paginationInfo?.prev_page_url as string)}
+													/>
+												</PaginationItem>
+											);
+										} else {
+											return (
+												<PaginationItem key={index}>
+													<PaginationLink
+														href="#"
+														isActive={link.active}
+														onClick={() => handlePageChange(link.url)}
+													>
+														{link.label}
+													</PaginationLink>
+												</PaginationItem>
+											);
+										}
+									})}
+								</PaginationContent>
+							</Pagination>
+						</div>
 					</CardContent>
 				</Card>
 			</section>
