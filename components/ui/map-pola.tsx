73303bd0cffc0023be229242b37bf4a74362aa9d
akbarsiddic <akbarsiddiq402@gmail.com>
2024-07-23 17:41:11 +0700

    fix home

--- components/ui/map-pola.tsx
+++ components/ui/map-pola.tsx
@@ -490,7 +490,7 @@ export default function MapPola({ flow }: MapProps) {
 					`}</style>
 				<img src="/location.svg" id="location" className="hidden" alt="" />
 				<Tabs defaultValue="all">
-					<section className="px-4 sm:px-8 md:px-50 pt-4 space-y-4 sm:space-y-8 md:space-y-20">
+					<section className="px-4 sm:px-8 md:px-10 lg:px-50 pt-4 space-y-4 sm:space-y-8 md:space-y-20">
 						<div className="flex flex-col sm:flex-row justify-between pt-10">
 							<div className="flex-col mb-3">
 								<h1 className="text-2xl sm:text-3xl md:text-4xl mb-3 font-extrabold">
