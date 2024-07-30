dda97c64adfc419ca5faae26adfae1a4e6c69324
akbarsiddic <akbarsiddiq402@gmail.com>
2024-07-30 19:53:12 +0700

    fix sidebar

--- components/admin/Sidebar/SidebarDropdown.tsx
+++ components/admin/Sidebar/SidebarDropdown.tsx
@@ -7,12 +7,12 @@ const SidebarDropdown = ({ item }: any) => {
 
   return (
 		<>
-			<ul className="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+			<ul className="mb-8 mt-8 flex flex-col gap-6 pl-6">
 				{item.map((item: any, index: number) => (
 					<li key={index}>
 						<Link
 							href={item.route}
-							className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-black duration-300 ease-in-out hover:text-white ${
+							className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-white duration-300 ease-in-out hover:text-white ${
 								pathname === item.route ? 'text-white' : ''
 							}`}>
 							{item.icon}
