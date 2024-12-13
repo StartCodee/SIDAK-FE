bd5fe06442ee09326f15a7319383031cf2fa46f4
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-12-13 22:17:30 +0700

    fixing admin be

--- app/admin/kecamatan/page.tsx
+++ app/admin/kecamatan/page.tsx
@@ -124,6 +124,21 @@ export default function Home() {
 			cell: (info) => info.row.index + 1,
 		},
 		{
+			accessorKey: 'kabupaten_name',
+			header: ({ column }) => {
+				return (
+					<Button
+						variant="ghost"
+						onClick={() =>
+							column.toggleSorting(column.getIsSorted() === 'asc')
+						}>
+						Nama Kabupaten
+						<ArrowUpDown className="ml-2 h-4 w-4" />
+					</Button>
+				);
+			},
+		},
+		{
 			accessorKey: 'name',
 			header: ({ column }) => {
 				return (
