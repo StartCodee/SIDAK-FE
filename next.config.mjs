8a38da14ce18951cc4674be018783ea74280fff1
KIGAMEKUN <68811531+Kigamekun@users.noreply.github.com>
2025-02-26 19:16:27 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api-sidak.startcode.id",
+      },
+    ],
+    unoptimized: true, // Matikan optimasi gambar bawaan Next.js
+  },
+};
 
 export default nextConfig;
